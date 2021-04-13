2bd09de63ae55edf233b41590626301e945898b7
Andrew059 <3503208@edu.externat.qc.ca>
2021-04-13 13:13:01 -0400

    First commit

---
+++ randomQuote.js
@@ -0,0 +1,22 @@
+let randomNumber = Math.floor(Math.random()* 10 + 1);
+if (randomNumber <= 1) {
+    console.log('Here is your random famous quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier." -Mother Teresa');
+} else if (randomNumber <= 2) {
+    console.log('Here is your random famous quote: "When you reach the end of your rope, tie a knot in it and hang on." -Franklin D. Roosevelt');
+} else if (randomNumber <= 3) {
+    console.log('Here is your random famous quote: "Always remember that you are absolutely unique. Just like everyone else." -Margaret Mead');
+} else if (randomNumber <= 4) {
+    console.log('Here is your random famous quote: "Dont judge each day by the harvest you reap but by the seeds that you plant." -Robert Louis Stevenson');
+} else if (randomNumber <= 5) {
+    console.log('Here is your random famous quote: "The future belongs to those who believe in the beauty of their dreams." -Eleanor Roosevelt');
+} else if (randomNumber <= 6) {
+    console.log('Here is your random famous quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn." -Benjamin Franklin');
+} else if (randomNumber <= 7) {
+    console.log('Here is your random famous quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart." -Helen Keller');
+} else if (randomNumber <= 8) {
+    console.log('Here is your random famous quote: "It is during our darkest moments that we must focus to see the light." -Aristotle');
+} else if (randomNumber <= 9) {
+    console.log('Here is your random famous quote: "Whoever is happy will make others happy too." -Anne Frank');
+} else {
+    console.log('Here is your random famous quote: "Do not go where the path may lead, go instead where there is no path and leave a trail." -Ralph Waldo Emerson');
+}
